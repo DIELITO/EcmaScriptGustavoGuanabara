4e7764dbe0b2f553db9570bdde40c5ea0d505218
DIELITO <d.a_ribeiro2@hotmail.com>
2022-08-20 18:59:16 -0400

    implementação do laço 'for' para realizar a contagem

--- aula014/ex017/script.js
+++ aula014/ex017/script.js
@@ -2,10 +2,27 @@ function contar(){
     var inicio = document.getElementById('ini')
     var fim = document.getElementById('fim')
     var passo = document.getElementById('pas')
+    var res = document.getElementById('res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         alert('[ERRO] Falta dados para fazer a contagem, verifique os campos!')
+        //document.location.reload(true)
     } else {
-        alert('tudo ok por enquanto')
+        res.innerHTML = '<br>Contando: '
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (i < f){
+            //loop para fazer a contagem crescente
+            for(var c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            //loop para fazer a contagem decrescente
+            for(var c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
