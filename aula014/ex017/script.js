c21075156d0cd2c891dac9d53488537cab9ed22f
DIELITO <d.a_ribeiro2@hotmail.com>
2022-08-20 19:18:36 -0400

    Tratamento de erros com campos null

--- aula014/ex017/script.js
+++ aula014/ex017/script.js
@@ -7,11 +7,16 @@ function contar(){
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         alert('[ERRO] Falta dados para fazer a contagem, verifique os campos!')
         //document.location.reload(true)
+        res.innerHTML = `<br>Impossível contar \u{1F614} Tente novamente \u{1F609}`
     } else {
-        res.innerHTML = '<br>Contando: '
+        res.innerHTML = '<br>Contando: <br>'
         var i = Number(inicio.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
+        if (p <= 0){
+            alert('Passo inválido! Vamos contar considerando "Passo: 1"')
+            p = 1
+        }
         if (i < f){
             //loop para fazer a contagem crescente
             for(var c = i; c <= f; c += p){
