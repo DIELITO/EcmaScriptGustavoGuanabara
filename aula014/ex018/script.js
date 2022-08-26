0eb5c33daa30d3359291c19ce9e3ac3f104ec30d
DIELITO <d.a_ribeiro2@hotmail.com>
2022-08-26 12:50:46 -0400

    gerando tabuada com while e mostrando no front com select

--- aula014/ex018/script.js
+++ aula014/ex018/script.js
@@ -1,10 +1,21 @@
 function tabuada(){
     var multiplicando = document.getElementById('multiplicando')
-    var multiplicador = 1
-    var res = ''
+    var multiplicador = document.getElementById('multiplicador')
+    var num = Number(multiplicando.value)
 
-    if (multiplicando == 0 || multiplicando.length == 0) {
-        alert('erro')
+    if (multiplicando.value.length == 0) {
+        alert('[ERRO] Por favor insira um numero para gerarmos a tabuada dele!')
+    } else if (num == 0) {
+        alert('[ATENÇÃO] Qualquer número multiplicado por "0" (zero) tem como resultado ZERO!')
+    } else {
+        var c = 1
+        multiplicador.innerHTML=''
+        while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c}`
+            multiplicador.appendChild(item)
+            c++
+        }
     }
 }
-//alert('olá')}
+//alert('até aqui ok!')}
