cde44d7694b5248bcc2d0486034002a0e8e93bba
DIELITO <d.a_ribeiro2@hotmail.com>
2022-08-26 11:28:14 -0400

    primeiro save da part backend

--- aula014/ex018/script.js
+++ aula014/ex018/script.js
@@ -1,1 +1,10 @@
-alert('olá')
+function tabuada(){
+    var multiplicando = document.getElementById('multiplicando')
+    var multiplicador = 1
+    var res = ''
+
+    if (multiplicando == 0 || multiplicando.length == 0) {
+        alert('erro')
+    }
+}
+//alert('olá')}
