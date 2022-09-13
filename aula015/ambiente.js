c3bc05a51f0e6bc665618d10b76a9098c0260fd2
DIELITO <d.a_ribeiro2@hotmail.com>
2022-09-13 10:19:55 -0400

    criando vetor

--- aula015/ambiente.js
+++ aula015/ambiente.js
@@ -1,3 +1,7 @@
-let num  = [5, 6, 7, 6]
+let num  = [5, 6, 7, 3, 9, 1, 2]
 
 console.log(`Nosso vetor é o ${num}`)
+console.log(num.length)
+console.log(num.sort())
+console.log(num.push(10))
+console.log(num)
